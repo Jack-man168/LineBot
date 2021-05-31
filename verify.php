7faa9dc56469f1696166437b8482d22cd28ead17
Jack-man168 <85101533+Jack-man168@users.noreply.github.com>
2021-05-31 21:06:15 +0800

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '#EKdZ6BeTdXO+dWiZh03f6QR4ZzNiNw9msTOV0E1I87iRLo2mjtWyjFMA9cTWAkopvKaywh00pSFx3XY9oEUnrZlfvddMDFIWrT3BjK2FrRgSiivESG2+OTAqUaM4s/CAJvTxPDJP3m9/gs6Wt5j5NAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'EKdZ6BeTdXO+dWiZh03f6QR4ZzNiNw9msTOV0E1I87iRLo2mjtWyjFMA9cTWAkopvKaywh00pSFx3XY9oEUnrZlfvddMDFIWrT3BjK2FrRgSiivESG2+OTAqUaM4s/CAJvTxPDJP3m9/gs6Wt5j5NAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
